1e91a162ee325ca13d8425e761e27395b3ce67d4
Brandon Rhinier <b.rhinier@gmail.com>
2021-10-02 18:17:25 -0500

    - Adding in start of the server task

---
+++ Server Hell/Assets/Scripts/Tasks/ServerTask/ServerTask.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerTask : UrgentTaskBase
+{
+	[SerializeField] private HoldInteractable serverIteractable;
+	[SerializeField] private float failTimer = 30f;
+
+	float currentTimer;
+
+	public void Awake()
+	{
+		serverIteractable.Interactable = false;
+		serverIteractable.ProgressComplete += ServerRepaired;
+	}
+
+	public override void ActivateTask()
+	{
+		base.ActivateTask();
+		currentTimer = failTimer;
+
+		serverIteractable.Interactable = true;
+	}
+
+	private void ServerRepaired()
+	{
+		IsActive = false;
+		serverIteractable.Interactable = false;
+		FireTaskComplete();
+	}
+
+	public void Update()
+	{
+		if (!IsActive) return;
+
+		currentTimer -= Time.deltaTime;
+
+		if(currentTimer <= 0)
+		{
+			currentTimer = failTimer;
+			FireTaskFailed();
+		}
+	}
+
+}
+
