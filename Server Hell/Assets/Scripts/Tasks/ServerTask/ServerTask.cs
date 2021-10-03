e3f9cf24ffe0e218e01cba6e5264ca7cb1326140
Brandon Rhinier <b.rhinier@gmail.com>
2021-10-02 19:08:47 -0500

    - Added flashing lights to the server.

--- Server Hell/Assets/Scripts/Tasks/ServerTask/ServerTask.cs
+++ Server Hell/Assets/Scripts/Tasks/ServerTask/ServerTask.cs
@@ -7,18 +7,36 @@ public class ServerTask : UrgentTaskBase
 	[SerializeField] private HoldInteractable serverIteractable;
 	[SerializeField] private float failTimer = 30f;
 
+	[SerializeField] private List<Light> lights;
+
+	private float lightsTimer;
+
 	float currentTimer;
 
 	public void Awake()
 	{
+		foreach (var light in lights)
+			light.color = Color.green;
+
+		lightsTimer = 1f;
 		serverIteractable.Interactable = false;
-		serverIteractable.ProgressComplete += ServerRepaired;	
+		serverIteractable.ProgressComplete += ServerRepaired;
+
+		ActivateTask();
 	}
 
 	public override void ActivateTask()
 	{
 		base.ActivateTask();
 		currentTimer = failTimer;
+
+		foreach (var light in lights)
+		{
+			light.color = Color.red;
+			light.enabled = true;
+		}
+
+		lightsTimer = 0.5f;
 
 		serverIteractable.Interactable = true;
 	}
@@ -28,20 +46,37 @@ public class ServerTask : UrgentTaskBase
 		IsActive = false;
 		serverIteractable.Interactable = false;
 		FireTaskComplete();
+
+		foreach (var light in lights)
+			light.color = Color.green;
 	}
 
 	public void Update()
 	{
+		UpdateLights();
+
 		if (!IsActive) return;
 
 		currentTimer -= Time.deltaTime;
 
-		if(currentTimer <= 0)
+		if (currentTimer <= 0)
 		{
 			currentTimer = failTimer;
 			FireTaskFailed();
 		}
 	}
 
+	private void UpdateLights()
+	{
+		lightsTimer -= Time.deltaTime;
+
+		if(lightsTimer <= 0f)
+		{
+			foreach (var light in lights)
+				light.enabled = !light.enabled;
+
+			lightsTimer = IsActive ? 0.5f : 2f;
+		}
+	}
 }
 
