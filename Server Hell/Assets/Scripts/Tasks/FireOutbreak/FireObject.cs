7a279b216f19a37bcd346882a44d248d0a84cd9b
Christian Bradley <cbradley@verboso.com>
2021-10-04 15:02:48 -0500

    -Added a steam sound when the fire gets hit by the extinguisher. -Disabled the fire extinguisher's stream hitbox by default.

--- Server Hell/Assets/Scripts/Tasks/FireOutbreak/FireObject.cs
+++ Server Hell/Assets/Scripts/Tasks/FireOutbreak/FireObject.cs
@@ -1,24 +1,48 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireObject : MonoBehaviour
 {
 	[SerializeField] private List<ParticleSystem> particles;
+    [SerializeField] private AdjustedAudioSource steamSource;
 
 	private bool active = true;
 	public event Action<FireObject> FireExtinquishedEvent;
 	
 	private float timeToExtinquish = 3f;
 	private float currentExtinquishTime = 0f;
+    private float timeBeforeStopSound = 1f;
+    private float curSteamStopTime = 0;
 
-	private void OnTriggerStay(Collider other)
+    private void Update()
+    {
+        if (active)
+        {
+            curSteamStopTime += Time.deltaTime;
+
+            if (curSteamStopTime >= timeBeforeStopSound && steamSource.source.isPlaying)
+                steamSource.Stop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        steamSource.Play();
+        curSteamStopTime = 0;
+    }
+
+    private void OnTriggerStay(Collider other)
 	{
 		if (!active || other.name != "StreamHitbox") return;
 
 		currentExtinquishTime += Time.deltaTime;
 		UpdateParticles();
+
+        if (!steamSource.source.isPlaying)
+            steamSource.Play();
+
+        curSteamStopTime = 0;
 	}
 
 	private void UpdateParticles()
@@ -26,6 +50,7 @@ public class FireObject : MonoBehaviour
 		if(currentExtinquishTime >= timeToExtinquish)
 		{
 			active = false;
+            steamSource.Stop();
 			foreach (var particleSys in particles)
 				particleSys.Stop();
 
